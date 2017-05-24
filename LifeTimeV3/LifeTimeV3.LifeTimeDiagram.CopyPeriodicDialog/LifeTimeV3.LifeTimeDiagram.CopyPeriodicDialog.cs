20170ba47fdcb92cfd8631fe1b8291df98b18698
Lars Becker <lars@lars-b.net>
2017-05-24 08:43:30 +0200

    Remove obsolete Counter Project

---
+++ LifeTimeV3/LifeTimeV3.LifeTimeDiagram.CopyPeriodicDialog/LifeTimeV3.LifeTimeDiagram.CopyPeriodicDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeTimeV3.LifeTimeV3.LifeTimeDiagram.CopyPeriodicDialog
+{
+    public partial class FormCopyPeriodicDialog : Form
+    {
+        public FormCopyPeriodicDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
