a6fb39fb8709fc8fc31b65f35ebb214c7dbdc1df
Lars Becker <lars@lars-b.net>
2017-05-24 13:22:38 +0200

    Work on copy periodic wizard

--- LifeTimeV3/LifeTimeV3.LifeTimeDiagram.CopyPeriodicDialog/LifeTimeV3.LifeTimeDiagram.CopyPeriodicDialog.cs
+++ LifeTimeV3/LifeTimeV3.LifeTimeDiagram.CopyPeriodicDialog/LifeTimeV3.LifeTimeDiagram.CopyPeriodicDialog.cs
@@ -16,8 +16,36 @@ namespace LifeTimeV3.LifeTimeDiagram.CopyPeriodicDialog
         #region properties
         public LifeTimeDiagramEditor.LifeTimeElement Object
         { get; set; }
+
         public List<LifeTimeDiagramEditor.LifeTimeElement> MultipliedObjectsCollection
         { get { return _multipliedObjects; } }
+
+        public enum PeriodBaseEnum { Days, Month, Years};
+
+        /// <summary>
+        /// Base unit of the period (every [Period] [Day/Month/Year])
+        /// </summary>
+        public PeriodBaseEnum PeriodBase { get; set; }
+
+        /// <summary>
+        /// Period (every x [BaseUnit])
+        /// </summary>
+        public int Period { get; set; }
+
+        /// <summary>
+        /// Ammount of copies to add
+        /// </summary>
+        public int AmmountOfCopies { get; set; }
+
+        /// <summary>
+        /// Limit until copies are made
+        /// </summary>
+        public DateTime LimitForAddingCopies { get; set; }
+
+        /// <summary>
+        /// Switch to use either Limit or Ammount for adding copies
+        /// </summary>
+        public bool UseLimitForAddingCopies { get; set; }
         #endregion
 
         #region fields
